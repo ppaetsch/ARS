e70667ff18c142f326844a42fcf3a49ead17bfc2
ppaetsch <P.Paetsch@campus.lmu.de>
2023-07-06 22:59:54 +0200

    Initial Commit

---
+++ src/test/java/com/uni/ARS/ArsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.ARS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
