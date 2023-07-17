f8383bc78f4ce80bdb5fa20f2874576f6ceea6cb
ppaetsch <P.Paetsch@campus.lmu.de>
2023-07-17 23:00:23 +0200

    prepared user login and question, with Server Sent Events

---
+++ src/main/java/com/uni/ARS/session/NotifyController.java
@@ -0,0 +1,50 @@
+package com.uni.ARS.session;
+
+import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@RestController
+public class NotifyController {
+
+    public List<SseEmitter> sseEmitterList = new CopyOnWriteArrayList<>();
+
+
+    @CrossOrigin
+    @GetMapping(value = "/notify", consumes = MediaType.ALL_VALUE)
+    public SseEmitter getEmitter(){
+        SseEmitter sseEmitter = new SseEmitter(Long.MAX_VALUE);
+        try {
+            sseEmitter.send(SseEmitter.event().name("INIT"));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        //sseEmitter.onCompletion(()-> sseEmitterList.remove(sseEmitter));
+        sseEmitterList.add(sseEmitter);
+        return sseEmitter;
+    }
+
+    @PostMapping(value = "/notifychange")
+    public String sendNotification(@RequestParam String name, Model model){
+        model.addAttribute("name", name);
+        return "userquestion.html";
+
+        /*for (SseEmitter emitter:sseEmitterList){
+            try {
+                emitter.send(SseEmitter.event().name("name").data(name));
+            } catch (IOException e){
+                //sseEmitterList.remove(emitter);
+                e.printStackTrace();
+            }
+        }*/
+
+    }
+
+
+}
---
+++ src/main/java/com/uni/ARS/session/UserController.java
@@ -0,0 +1,34 @@
+package com.uni.ARS.session;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private ARSSessionHandler arsSessionHandler;
+
+    @GetMapping("/Session/{sessionname}")
+    public String getUserLogin(@PathVariable String sessionname, Model model){
+        if(arsSessionHandler.getArsSessions().containsKey(sessionname)){
+            model.addAttribute("sessionname", sessionname);
+            return "userlogin.html";
+        }
+        return "error.html";
+    }
+
+    @PostMapping("/Session/userlogin")
+    public String acceptUserLogin(@RequestParam(name="name") String name, @RequestParam(name="sessionname") String sessionname, Model model){
+        ARSSession2 arsSession2 = arsSessionHandler.getSession(sessionname);
+        arsSession2.users.add(name);
+        model.addAttribute("user", name);
+        model.addAttribute("sessionname", sessionname);
+        return "userquestion.html";
+    }
+}
